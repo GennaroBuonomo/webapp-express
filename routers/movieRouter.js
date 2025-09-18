2150060af497520bd5027e285301b1ddad8d8973
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-09-18 17:30:28 +0200

    importo il router express e controller

---
+++ routers/movieRouter.js
@@ -0,0 +1,9 @@
+//IMPORTO EXPRESS
+const express = require('express');
+
+//IMPORTO ROUTER
+const router = express.Router();
+
+//IMPORTO IL CONTROLLER
+const movieController = require("../controllers/movieController");
+
