0abf352a6f4aa1f6e1cf646e6aa598193f4df8e8
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-09-18 17:16:09 +0200

    Inserimento ai comandi start e watch il file .env

--- data/db.js
+++ data/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 
 //CREO LA CONNESSIONE
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST || "boolean",
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
