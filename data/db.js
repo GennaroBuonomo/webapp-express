157c62c54d2b8ef69359e8504ff7e80da7e5d0e5
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-09-18 16:56:10 +0200

    stabilisco la connessione

--- data/db.js
+++ data/db.js
@@ -7,4 +7,14 @@ const connection = mysql.createConnection({
   user: "root",
   password: "Root",
   database: "db_movies"
+});
+
+//STABILISCO LA CONNESSIONE
+connection.connect((err) => {
+  if(err){
+    console.log(`Errore nella connessione al db: ${err}`)
+  }
+  else{
+    console.log("Connessione al db avvenuta correttamente")
+  }
 })
